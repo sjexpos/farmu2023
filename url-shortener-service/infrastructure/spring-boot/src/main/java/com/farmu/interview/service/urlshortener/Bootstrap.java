c00cd49d7bcaa00d8733b14b42434925887446f5
Sergio Exposito <sergio.exposito@teracode.com>
2023-02-17 16:56:21 -0300

    Fix bootstrap main class

--- url-shortener-service/infrastructure/spring-boot/src/main/java/com/farmu/interview/service/urlshortener/Bootstrap.java
+++ url-shortener-service/infrastructure/spring-boot/src/main/java/com/farmu/interview/service/urlshortener/Bootstrap.java
@@ -12,30 +12,4 @@ public class Bootstrap {
 		SpringApplication.run(Bootstrap.class, args);
 	}
 
-    // @Bean
-    // public ApplicationRunner initializeData(UserJpaRepository plantRepository) throws Exception {
-    //     return (ApplicationArguments args) -> {
-    //         List<User> plants = Arrays.asList(
-    //                 new User("subalpine fir", "abies lasiocarpa", "pinaceae"),
-    //                 new User("sour cherry", "prunus cerasus", "rosaceae"),
-    //                 new User("asian pear", "pyrus pyrifolia", "rosaceae"),
-    //                 new User("chinese witch hazel", "hamamelis mollis", "hamamelidaceae"),
-    //                 new User("silver maple", "acer saccharinum", "sapindaceae"),
-    //                 new User("cucumber tree", "magnolia acuminata", "magnoliaceae"),
-    //                 new User("korean rhododendron", "rhododendron mucronulatum", "ericaceae"),
-    //                 new User("water lettuce", "pistia", "araceae"),
-    //                 new User("sessile oak", "quercus petraea", "fagaceae"),
-    //                 new User("common fig", "ficus carica", "moraceae")
-    //         );
-    //         plantRepository.saveAll(plants);
-    //     };
-    // }
-
-    // @Bean
-    // public ApplicationRunner buildIndex(Indexer indexer) throws Exception {
-    //     return (ApplicationArguments args) -> {
-    //         indexer.indexPersistedData(User.class);
-    //     };
-    // }
-	
 }
